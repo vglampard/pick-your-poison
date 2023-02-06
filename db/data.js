40fb574b2a9792377ce1f8b5e96939d687673b7f
BushraFatimaBF <vglampard@protonmail.com>
2023-02-06 15:38:39 +0000

    added seed data

--- db/data.js
+++ db/data.js
@@ -2,7 +2,51 @@ export const sessions = [
 
 {
     date: 2023-01-29,
-    
-
+    headache: 2,
+    nausea: 4,
+    fatigue: 7
+},
+{
+    date: 2023-02-02,
+    headache: 9,
+    nausea: 9,
+    fatigue: 9
+},
+{
+    date: 2023-01-20,
+    headache: 1,
+    nausea: 0,
+    fatigue: 3
 }
+]
+
+export const drinks = [
+
+{
+date: 2023-01-29,
+wine: 2,
+beer: 0,
+spirit: 1,
+cider: 0,
+alcopop: 0,
+},
+
+{
+    date: 2023-02-02,
+    wine: 2,
+    beer: 0,
+    spirit: 1,
+    cider: 0,
+    alcopop: 0,
+    },
+    
+{
+    date: 2023-01-20,
+    wine: 0,
+    beer: 3,
+    spirit: 1,
+    cider: 0,
+    alcopop: 0,
+    }
+
 ]
--- db/scripts/reset-tables.js
+++ db/scripts/reset-tables.js
@@ -4,14 +4,14 @@ import pool from "../index.js"
 // CREATE sessions table
 export async function createSessionsTable() {
     await pool.query(
-      "CREATE TABLE sessions(id INT GENERATED ALWAYS AS IDENTITY, date DATE PRIMARY KEY, headache INT, nausea INT, fatigue INT, general INT;)"
+      "CREATE TABLE sessions(id INT GENERATED ALWAYS AS IDENTITY, date DATE PRIMARY KEY, headache INT, nausea INT, fatigue INT);"
     );
   }
 
  // CREATE drinks table
 export async function createDrinksTable (){
     await pool.query(
-      "CREATE TABLE drinks(id INT GENERATED ALWAYS AS IDENTITY, date DATE PRIMARY KEY, wine INT, beer INT, spirit INT, cider INT, alcopop INT;)"
+      "CREATE TABLE drinks(id INT GENERATED ALWAYS AS IDENTITY, date DATE PRIMARY KEY, wine INT, beer INT, spirit INT, cider INT, alcopop INT);"
     );
 }  
 
